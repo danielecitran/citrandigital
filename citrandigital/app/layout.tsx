5848429605f3601f999ee614f5479c1bcc320394
danielecitran <daniele.ctr.zh@icloud.com>
2026-03-21 19:28:10 +0100

    add vercel analytics

--- citrandigital/app/layout.tsx
+++ citrandigital/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Geist_Mono, Instrument_Sans, Plus_Jakarta_Sans } from "next/font/google";
 import { headers } from "next/headers";
 
@@ -60,6 +61,7 @@ export default async function RootLayout({
         className={`${instrumentSans.className} min-h-full flex flex-col antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
